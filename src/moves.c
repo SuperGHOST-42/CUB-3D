c2f0d643cd24352f7414dfbb48ad87d2b66516e9
Ariclenes <ariclenesp12@gmail.com>
2026-05-04 00:40:45 +0100

    restore

--- src/moves.c
+++ src/moves.c
@@ -6,7 +6,7 @@
 /*   By: arpereir <arpereir@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 19:00:29 by arpereir          #+#    #+#             */
-/*   Updated: 2026/05/03 23:41:14 by arpereir         ###   ########.fr       */
+/*   Updated: 2026/05/04 00:05:34 by arpereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,8 +57,8 @@ void	move_left(t_game *game)
 	double	speed;
 
 	speed = SPEED;
-	new_x = game->player.x - (game->player.plane_x * speed);
-	new_y = game->player.y - (game->player.plane_y * speed);
+	new_x = game->player.x - (game->player.fov_x * speed);
+	new_y = game->player.y - (game->player.fov_y * speed);
 	if (can_move(game, new_x, game->player.y))
 		game->player.x = new_x;
 	if (can_move(game, game->player.x, new_y))
@@ -72,8 +72,8 @@ void	move_right(t_game *game)
 	double	speed;
 
 	speed = SPEED;
-	new_x = game->player.x + (game->player.plane_x * speed);
-	new_y = game->player.y + (game->player.plane_y * speed);
+	new_x = game->player.x + (game->player.fov_x * speed);
+	new_y = game->player.y + (game->player.fov_y * speed);
 	if (can_move(game, new_x, game->player.y))
 		game->player.x = new_x;
 	if (can_move(game, game->player.x, new_y))
@@ -92,11 +92,11 @@ void	rotate_left(t_game *game)
 		- game->player.dir_y * sin(rot_speed);
 	game->player.dir_y = old_dir_x * sin(rot_speed)
 		+ game->player.dir_y * cos(rot_speed);
-	old_plane_x = game->player.plane_x;
-	game->player.plane_x = game->player.plane_x * cos(rot_speed)
-		- game->player.plane_y * sin(rot_speed);
-	game->player.plane_y = old_plane_x * sin(rot_speed)
-		+ game->player.plane_y * cos(rot_speed);
+	old_plane_x = game->player.fov_x;
+	game->player.fov_x = game->player.fov_x * cos(rot_speed)
+		- game->player.fov_y * sin(rot_speed);
+	game->player.fov_y = old_plane_x * sin(rot_speed)
+		+ game->player.fov_y * cos(rot_speed);
 }
 
 void	rotate_right(t_game *game)
@@ -111,9 +111,9 @@ void	rotate_right(t_game *game)
 		- game->player.dir_y * sin(-rot_speed);
 	game->player.dir_y = old_dir_x * sin(-rot_speed)
 		+ game->player.dir_y * cos(-rot_speed);
-	old_plane_x = game->player.plane_x;
-	game->player.plane_x = game->player.plane_x * cos(-rot_speed)
-		- game->player.plane_y * sin(-rot_speed);
-	game->player.plane_y = old_plane_x * sin(-rot_speed)
-		+ game->player.plane_y * cos(-rot_speed);
+	old_plane_x = game->player.fov_x;
+	game->player.fov_x = game->player.fov_x * cos(-rot_speed)
+		- game->player.fov_y * sin(-rot_speed);
+	game->player.fov_y = old_plane_x * sin(-rot_speed)
+		+ game->player.fov_y * cos(-rot_speed);
 }
